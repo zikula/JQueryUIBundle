f18d7775d0a1e668fef66aca11bf093afb8ea6ed
cmfcmf <cmfcmf.flach@gmail.com>
2013-08-16 15:41:27 +0200

    Added bundle and composer file.

---
+++ ZikulaJQueryUIBundle.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Copyright Zikula Foundation 2013 - Zikula jQuery UI bundle
+ *
+ * This work is contributed to the Zikula Foundation under one or more
+ * Contributor Agreements and licensed to You under the following license:
+ *
+ * @license GNU/LGPLv3 (or at your option, any later version).
+ * @package Zikula
+ *
+ * Please see the NOTICE file distributed with this source code for further
+ * information regarding copyright and licensing.
+ */
+
+namespace Zikula\JQueryUIBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ZikulaJQueryUIBundle extends Bundle
+{
+
+}
